ef54d97a753f184df15f553860123e6fbf8b61ca
Facundo Cubells <facundocubells@hotmail.com>
2022-06-09 23:31:25 -0300

    Added a local .json API to connect via CardProps

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Cardv1 } from '../index'
 import './cardlist.css'
-import{ img1, img2, img3 } from '../assets'
+import{ img1, img2, img3, info } from '../assets'
 
 function CardList() {
   return (
     <div className="container-carrousel">
         <ul className='horizontal-list'>
-            <Cardv1 image={img1} nombre='Callate' texto="El club de los perros es mas para gente que le gusta lamer el piso y comer sobras de los demas Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, similique? Illum consectetur architecto quo accusamus commodi consequuntur atque animi aut?.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, similique? Illum consectetur architecto "/>
-            <Cardv1 image={img2} nombre='Gato' texto="El club de los Gatos liderado por el Gato mas gatuno lo conforma mas de 1000 gatitos Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, similique? Illum consectetur architecto quo accusamus commodi consequuntur atque animi aut? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, similique? Illum consectetur architecto quo  "/>
-            <Cardv1 image={img3} nombre='Micho' texto="El club de Micho es el mas michifus Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, similique? Illum consectetur architecto quo accusamus commodi consequuntur atque animi aut? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, similique? Illum consectetur architecto quo accusamus commodi consequuntur atque animi aut?"/>
+            <Cardv1 image={img1} nombre={info.interact.nombre} texto={info.interact.texto}/>
+            <Cardv1 image={img2} nombre={info.rotaract.nombre} texto={info.rotaract.texto}/>
+            <Cardv1 image={img3} nombre={info.rotary.nombre} texto={info.rotary.texto}/>
         </ul>
     </div>
   )
--- src/Components/assets/index.js
+++ src/Components/assets/index.js
@@ -1,11 +1,12 @@
 import img1 from "./img1.jpg";
 import img2 from "./img3.jpg";
-import img3 from "./michoputo.jpg";
+import img3 from "./img2.jpg";
+import info from "./info.json"
 
 
 export {
     img1,
     img2,
     img3,
-
+    info,
 }
